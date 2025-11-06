e1c112a878ee862fe7aebf065d28a3fada9e6c18
masterJKR <nilzero25@gamil.com>
2025-11-06 12:56:16 +0900

    11월 6일 시간표

--- reactStudy/src/component/study2/ScheduleList.jsx
+++ reactStudy/src/component/study2/ScheduleList.jsx
@@ -1,8 +1,20 @@
 // ScheduleList.jsx
 
-export default function ScheduleList(){
+export default function ScheduleList({items}){
+    if(items.length === 0)
+        return <p>등록된 일정이없습니다.</p>;
     return(
-        <>
-        </>
+        <ul style={{padding:0}}>
+            {
+                items.map(
+                    ( {id, time, text} ) => (
+                        <li key={id} style={{ marginBottom:6}}>
+                            <strong style={{ width: 70, display: "inline-block"}}>{time}</strong>
+                            <span >{text}</span>
+                        </li>
+                    )
+                )
+            }
+        </ul>
     );
 }
