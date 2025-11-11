d309669142409ebc5f44aa248ea3652f0138a6cb
masterJKR <nilzero25@gamil.com>
2025-11-11 17:30:33 +0900

    11월 11일

--- datachart/src/App.jsx
+++ datachart/src/App.jsx
@@ -2,32 +2,39 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ChartBasic1 from './component/ChartBasic1'
+import BarChart1 from './component/BarChart1'
+import BarChart2 from './component/BarChart2'
+import BarChart3 from './component/BarChart3'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ show, setShow ] = useState(null); 
+
+  const btClass = (name) => 
+        `px-4 py-2 rounded-lg text-white font-bold transition cursor-pointer
+          ${show === name ? "bg-blue-600" : "bg-blue-400 hover:bg-blue-500"}`;
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className='flex gap-3'>
+        <button onClick={ () => setShow("A") } className={ btClass("A") }>차트둘러보기</button>
+        <button onClick={ () => setShow("B") } className={ btClass("B") }>
+            막대그래프1
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={ () => setShow("C") } className={ btClass("C") }>
+            막대그래프2
+        </button>
+        <button onClick={ () => setShow("D") } className={ btClass("D") }>
+            누적막대
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className='mt-8 '>
+        { show==="A" &&  <ChartBasic1 /> }
+        { show==="B" &&  <BarChart1 /> }
+        { show==="C" &&  <BarChart2 /> }
+        { show==="D" &&  <BarChart3 /> }
+      </div>
     </>
   )
 }
---
+++ datachart/src/component/BarChart1.jsx
@@ -0,0 +1,42 @@
+// BarChart1.jsx
+
+import "chart.js/auto"
+import { Bar } from "react-chartjs-2"
+import { useState } from "react";
+
+export default function BarChart1(){
+    const score = [
+        {name:"이순신", avg : 87 }, { name :"강감찬", avg:98 },
+        {name:"장보고", avg:66 }, {name:"정도전", avg:99} ,
+        {name:"정약용", avg:81 }, {name:"한석봉", avg:54}
+    ];
+
+    const data={
+        labels :  score.map( (u) => u.name ) ,
+        datasets : [
+            {
+                label :"기말성적",
+                data : score.map( (u) => u.avg ) ,
+                backgroundColor:"#FF0000"
+            }
+        ]
+    };
+
+    const options={
+        indexAxis: "y"
+    };
+
+// 가로 막대와 세로 막대  
+//  가로 막대  :  항목간의 크기 비교 강조 할때 사용
+//              주로  부서별 인원수, 제품별 만족도, 순위, 이름 목록 등 데이터표현 할때 사용
+//             가로막대를 화면에 표시라는 방향은 위->아래  로 한다.
+//  세로막대  :  시간, 순서, 트렌드 강조
+//             월별 매출, 연도별 증가율, 시계열 데이터 
+//            세로막대를 화면에 표시하는 방향은 왼 -> 오 로 한다.
+
+    return(
+        <>
+            <Bar data={data}  options={options} />
+        </>
+    );
+}
