b37a0e504787084eabcf4cd4e758bd51c6697f2e
masterJKR <nilzero25@gamil.com>
2025-11-17 14:25:08 +0900

    11월 14일 파이어베이스

--- reactdata/src/page/TodoPage.jsx
+++ reactdata/src/page/TodoPage.jsx
@@ -1,8 +1,18 @@
 // TodoPage.jsx
 
+import TodoForm from "../component/TodoForm";
+import TodoList from "../component/TodoList";
 
 export default function TodoPage(){
     return(
-        <></>
+        <div className="mx-auto max-w-xl p-10 space-y-4">
+            <h1 className="text-xl text-white"> 
+                오늘 할일 with 파이어베이스 
+            </h1>
+         
+            <TodoForm />
+            <TodoList/>
+            
+        </div>
     );
 }
